d5b2152fe16395e9134a34a3c23d579e1e70d7be
wonchul98 <shinwc1998@naver.com>
2023-08-05 14:38:11 +0900

    Update api_test.py

--- medisign/medicines/tests/api_test.py
+++ medisign/medicines/tests/api_test.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-url = "http://127.0.0.1:8000/medicines/auth"
+url = "https://medisign-hackthon-95c791df694a.herokuapp.com/medicines/auth"
 
 response = requests.post(url, data = {'username': 'admin', 'password' : '1'})
 myToken = response.json()['token'] # JSON 형식에서 토큰 추출
 print(myToken)
 
 header = {'Authorization': 'Token '+ myToken} # 'Token'을 사용
-response = requests.get('http://127.0.0.1:8000/medicines/Medicine_list', headers = header)
+response = requests.get('https://medisign-hackthon-95c791df694a.herokuapp.com/medicines/Medicine_list', headers = header)
 print(response.text)
