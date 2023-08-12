81e319dd7945548d0f6b791be96de1d145bc0098
wonchul98 <shinwc1998@naver.com>
2023-08-12 16:44:08 +0900

    admin권한 추가

--- medisign/diseases/admin.py
+++ medisign/diseases/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Disease
+
+@admin.register(Disease)
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    
--- medisign/pharmacies/admin.py
+++ medisign/pharmacies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Pharmacy
 
-# Register your models here.
+class PharmacyAdmin(admin.ModelAdmin):
+    list_display = ['care_institution_name', 'city_name', 'district_name', 'phone_number', 'address']
+    search_fields = ['care_institution_name', 'city_name', 'district_name', 'phone_number', 'address']
+    list_filter = ['city_name', 'district_name']
+    ordering = ['care_institution_name']
+
+
+admin.site.register(Pharmacy, PharmacyAdmin)
+    
