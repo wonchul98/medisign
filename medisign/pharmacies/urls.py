814db85e0ca8974c12d13a1f69d4e2f5fffc74dc
wonchul98 <shinwc1998@naver.com>
2023-08-18 16:21:52 +0900

    약국 지도 조회 추가

--- medisign/pharmacies/urls.py
+++ medisign/pharmacies/urls.py
@@ -11,11 +11,12 @@ urlpatterns = [
     # path("pharmacy_list", PharmacyInfo.as_view(), name="pharmacy_list"),  # 불안정함
     # pharmacies/nearby?lat=37.50&lon=127.50&distance_km=1
     path("nearby", PharmacyNearbyView.as_view(), name='nearby'), # 반경 내 약국
+    # pharmacies/show_reg?user_id=
+    path('reg/<int:user_id>/', RegularPharmacyView.as_view(), name='regular_pharmacy'),
     # pharmacies/show_near?lat=37.3595704&lon=127.105399&distance_km=10
     path('show_near/', TemplateView.as_view(template_name='pharmacies/near_pharmacies.html'), name='show_near'),
-    # pharmacies/show_reg?user_id=
-    path('show_reg/<int:user_id>/', RegularPharmacyView.as_view(), name='show_regular_pharmacy'),
-
-    
-    
+    # pharmacies/show_select?pharmact_id=200
+    path('show_select/', TemplateView.as_view(template_name='pharmacies/select_pharmacies.html'), name='show_select'),
+    # pharmacies/detail/20
+    path('detail/<int:pharmacy_id>/', views.PharmacyDetail.as_view(), name='pharmacy-detail'),
 ]
--- medisign/pharmacies/views.py
+++ medisign/pharmacies/views.py
@@ -3,19 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 import requests
 import xml.etree.ElementTree as ET
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import PharmacySerializer
 from .models import Pharmacy
-
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
+class PharmacyDetail(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self, request, pharmacy_id):
+        try:
+            pharmacy = Pharmacy.objects.get(pk=pharmacy_id)
+            serializer = PharmacySerializer(pharmacy, context={'request': request})
+            return Response(serializer.data)
+        except Pharmacy.DoesNotExist:
+            return Response({"error": "Pharmacy not found"}, status=status.HTTP_404_NOT_FOUND)
 
 class PharmacyNearbyView(APIView):
     permission_classes = [AllowAny]
